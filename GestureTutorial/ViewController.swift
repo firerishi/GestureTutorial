45d9ab84b7f4eb7d59bf03b3824098b68f8e2a19
Srinivas, Thirumalaa <SrinivaT@advisory.com>
2015-02-18 07:49:18 -0600

    Initial Commit

---
+++ GestureTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GestureTutorial
+//
+//  Created by Srinivas, Thirumalaa on 2/18/15.
+//  Copyright (c) 2015 Srinivas, Thirumalaa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
